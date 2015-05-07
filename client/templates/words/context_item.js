9324b5bba8a720cb9df3b28caa2c3ad02283e1a9
Charles Zhu <charlesmzhu@gmail.com>
2015-05-07 16:55:58 -0700

    Done with adding contexts and now using a router-based means of getting where I need to go. Weird logs when I refresh + refreshing on an individual post gets rid of the currentArray

--- client/templates/words/context_item.js
+++
@@ -1,5 +0,0 @@
-Template.contextItem.helpers ({
-	context: function () { 
-		return Session.get ('wordArray')[0].context;
-	}
-})
--- client/templates/words/word_page.js
+++ client/templates/words/word_page.js
@@ -1,16 +1,35 @@
-Template.wordPage.helpers ({//Need to get this to return a word with
-	word: function ( index ) {
-		return Session.get ( 'wordArray' ) [ index ] ;
+Template.wordPage.events ( { 
+
+	"click .arrow-right": function ( e ) { // As long as there's another
+		if ( Words.state.indexOf ( this._id ) < Words.state.currentSet.length ) {	
+			var nextId = Words.state.currentSet [ Words.state.indexOf (this._id) + 1 ]._id;
+			Router.go("/" + nextId);
+		}
 	},
 
-	index: function () { return Session.get ( 'index' ); },
-})
-
-Template.wordPage.events ( { 
-	"click #arrow-right": function ( e ) {
-		if ( Session.get ('index') < Session.get ('wordArray').length-1 ) {
-			console.log(Session.get('index'));
-			Session.set ( 'index', Session.get ( 'index' ) + 1);
+	"click .arrow-left": function ( e ) { // As long as there's another
+		if ( Words.state.indexOf ( this._id ) > 0 ) {	
+			var prevId = Words.state.currentSet [ Words.state.indexOf (this._id) - 1 ]._id;
+			Router.go("/" + prevId);
 		}
+	},
+
+	"submit #submit-context": function ( e ) {
+		var text = event.target.text.value;
+		Meteor.call ( "addContext" , this._id, text )
+
+		event.target.text.value = "";
+
+		//Meteor.call ( this._id, text );
+		return false;
 	}
 } )
+
+Template.wordPage.helpers ({
+	wordsInFront: function () { 
+		return Words.state.indexOf (this._id) < Words.state.length -1;
+	},
+	wordsInBack: function () {
+		return Words.state.indexOf (this._id) > 0;
+	}
+})
